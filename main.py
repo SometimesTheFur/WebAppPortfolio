e30396142fefe86a9a9e2b7670d262e7604e2125
sometimesthefur <timothymahr@gmail.com>
2023-01-17 20:30:59 -0500

    New Projects with columns and img folder

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
+import json
+import requests
 import streamlit as st
+from streamlit_lottie import st_lottie
 
 st.set_page_config(page_title="Timothy Mahr", page_icon=":rocket:", layout="wide")
+
+def load_lottieurl(url: str):
+    r = requests.get(url)
+    if r.status_code != 200:
+        return None
+    return r.json()
+
+
+# ----LOAD ASSETS----
+animation1 = load_lottieurl("https://assets10.lottiefiles.com/private_files/lf30_pljwgbzs.json")
 
 # ----HEADER----
 with st.container():
@@ -28,5 +41,18 @@ with st.container():
             Not always in that order!
             """
         )
+with right_column:
+    st_lottie(animation1, height=500, key="coding")
 
+# ----PROJECTS----
+with st.container():
+    st.write("---")
+    st.header("My projects")
+    st.write("##")
+    image_column, discription_column = st.columns((1, 2))
 
+    with image_column:
+        st.write("imggoeshere")
+    with discription_column:
+        st.subheader("Interactive")
+        st.write("Some simple and fun interactive toys, puzzles, and games I've built using JS, Java, and Python")
